84bec2afc794c1b39350cef1c5efe0c2fb57fd31
josephxnassar <josephxnassar@outlook.com>
2026-04-05 14:53:44 -0700

    Small frontend adjustments.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+    return (
+        <div>
+            <button className='statistics-button'>Statistics</button>
+            <button className='schedules-button'>Schedules</button>
+            <button className='depthcharts-button'>DepthCharts</button>
+        </div>
+    )
 }
 
 export default App
